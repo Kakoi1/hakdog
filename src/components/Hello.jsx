b810b18e342f63e93a4a42afe690a12044d7cf11
Kakoi1 <lopezrolandshane@gmail.com>
2024-07-13 11:00:33 +0800

    hakdog

---
+++ src/components/Hello.jsx
@@ -0,0 +1,22 @@
+function Hello() {
+  return (
+    <div>
+      <h1>Helo</h1>
+      <Former></Former>
+    </div>
+  );
+}
+function Former() {
+  return (
+    <div>
+      <h1>form</h1>
+      <input type="text" name="" id="" />
+      <br />
+      <input type="password" name="" id="" />
+      <br />
+      <textarea name="" id=""></textarea>
+    </div>
+  );
+}
+
+export default Hello;
